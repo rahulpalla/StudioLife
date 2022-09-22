0610cc6d2cc1e64e2ff357593c793b7ee03a1f03
shvetkat <s.rajkatta02@gmail.com>
2022-09-22 17:22:38 -0500

    soem attempts

--- app/src/main/java/com/example/studiolife/EntryFormActivity.kt
+++ app/src/main/java/com/example/studiolife/EntryFormActivity.kt
@@ -1,17 +1,43 @@
+/*
+* Copyright (C) 2021 The Android Open Source Project.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.example.studiolife
+
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.Fragment
-import com.example.studiolife.databinding.ActivityMainBinding
+import com.example.studiolife.QuestionCardAdapter
 import com.example.studiolife.databinding.EntryFormBinding
+import com.example.studiolife.databinding.EntryFormFullBinding
 
-class EntryFormActivity : Fragment(R.layout.entry_form) {
+class EntryFormActivity : AppCompatActivity() {
 
-    private lateinit var binding: EntryFormBinding
+    private lateinit var binding: EntryFormFullBinding
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding = EntryFormBinding.bind(view)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = EntryFormFullBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.verticalRecyclerView.adapter = QuestionCardAdapter(
+            applicationContext,
+        )
+
+        // Specify fixed size to improve performance
+        binding.verticalRecyclerView.setHasFixedSize(true)
+
+        // Enable up button for backward navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
