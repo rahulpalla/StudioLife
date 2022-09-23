5dd073fbc91151662a05eff12be3be2407a080d2
shvetkat <s.rajkatta02@gmail.com>
2022-09-22 20:50:28 -0500

    button stuff - stil not working fs

--- app/src/main/java/com/example/studiolife/QuestionCardAdapter.kt
+++ app/src/main/java/com/example/studiolife/QuestionCardAdapter.kt
@@ -1,15 +1,20 @@
 package com.example.studiolife
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.studiolife.DataSource.questions
 import com.example.studiolife.QuestionCardAdapter
 import com.example.studiolife.R
 import com.example.studiolife.DataSource
+import kotlinx.coroutines.NonDisposableHandle.parent
 
 class QuestionCardAdapter (
     private val context: Context?
@@ -17,6 +22,7 @@ class QuestionCardAdapter (
 
     //Initialize the data using the List found in data/DataSource
     private val qList = questions
+    private var myData: ArrayList<String> = ArrayList()
 
     /**
      * Initialize view elements
@@ -27,6 +33,7 @@ class QuestionCardAdapter (
         val input1Text: TextView? = view!!.findViewById(R.id.input1_edit_text)
         val input2Text: TextView? = view!!.findViewById(R.id.input2_edit_text)
         val input3Text: TextView? = view!!.findViewById(R.id.input3_edit_text)
+        val enterButton: Button = view!!.findViewById(R.id.enter_btn)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionCardViewHolder {
@@ -42,9 +49,41 @@ class QuestionCardAdapter (
     override fun onBindViewHolder(holder: QuestionCardViewHolder, position: Int) {
         // Get the data at the current position
         val data = qList[position]
+        var inputOne: String = ""
+        var inputTwo: String = ""
+        var inputThree: String = ""
         holder.questionText?.text = data.question
-        holder.input1Text?.text = data.input1
-        holder.input2Text?.text = data.input2
-        holder.input3Text?.text = data.input3
+//        holder.input1Text?.text = data.input1
+//        holder.input2Text?.text = data.input2
+//        holder.input3Text?.text = data.input3
+        holder.input1Text?.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                inputOne = s.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        holder.input2Text?.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                inputTwo = s.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        holder.input3Text?.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                inputThree = s.toString()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+        holder.enterButton?.setOnClickListener() {
+            Toast.makeText(context, "yo", Toast.LENGTH_LONG)
+            myData.add(inputOne)
+            myData.add(inputTwo)
+            myData.add(inputThree)
+        }
     }
 }
